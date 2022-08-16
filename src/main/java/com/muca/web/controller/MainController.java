bcd78b72ecefff133e8e9a21ccc562c25f155540
jaejung Kim <muca0127@gmail.com>
2022-08-16 21:55:46 +0900

    베이스 프로젝트 수정

---
+++ src/main/java/com/muca/web/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.muca.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String main() {
+        log.debug("main page");
+        return "index";
+    }
+}
