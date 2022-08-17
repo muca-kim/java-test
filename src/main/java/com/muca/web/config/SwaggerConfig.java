21069593870fdf61a2bb3b3444b338c890010fe9
jaejung Kim <muca0127@gmail.com>
2022-08-17 23:40:41 +0900

    스프링부트 버전에 따른 swagger 변경

--- src/main/java/com/muca/web/config/SwaggerConfig.java
+++ src/main/java/com/muca/web/config/SwaggerConfig.java
@@ -1,35 +1,34 @@
-package com.muca.web.config;
+// package com.muca.web.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+// import springfox.documentation.builders.ApiInfoBuilder;
+// import springfox.documentation.builders.PathSelectors;
+// import springfox.documentation.builders.RequestHandlerSelectors;
+// import springfox.documentation.service.ApiInfo;
+// import springfox.documentation.spi.DocumentationType;
+// import springfox.documentation.spring.web.plugins.Docket;
 
-@EnableSwagger2
-@Configuration
-public class SwaggerConfig {
+// // @EnableSwagger2
+// @Configuration
+// public class SwaggerConfig {
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("v1")
-                .apiInfo(this.apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.muca.web"))
-                .paths(PathSelectors.any())
-                .build();
-    }
+// @Bean
+// public Docket api() {
+// return new Docket(DocumentationType.OAS_30)
+// .groupName("v1")
+// .apiInfo(this.apiInfo())
+// .select()
+// .apis(RequestHandlerSelectors.basePackage("com.muca.web"))
+// .paths(PathSelectors.any())
+// .build();
+// }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("api.jpa")
-                .description("jpa 연습")
-                .build();
-    }
-}
+// private ApiInfo apiInfo() {
+// return new ApiInfoBuilder()
+// .title("api.jpa")
+// .description("jpa 연습")
+// .build();
+// }
+// }
